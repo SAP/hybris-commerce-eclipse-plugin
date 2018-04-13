729cb40582d1eeecd83edcaa6694eea8bc4c10e4
Roy, Cameron <cameron.roy@sap.com>
2018-04-13 16:24:36 -0700

    Innos-742: Import extensions, update localextensions.xml and fix classpath. Done.

--- com.hybris.hyeclipse.importer/src/com/hybris/yps/hyeclipse/wizards/ImportExtensionWizard.java
+++ com.hybris.hyeclipse.importer/src/com/hybris/yps/hyeclipse/wizards/ImportExtensionWizard.java
@@ -3,17 +3,27 @@ package com.hybris.yps.hyeclipse.wizards;
 import org.eclipse.ui.internal.ide.IDEWorkbenchPlugin;
 import org.xml.sax.SAXException;
 
+import com.hybris.yps.hyeclipse.Activator;
 import com.hybris.yps.hyeclipse.utils.FixProjectsUtils;
+import com.hybris.yps.hyeclipse.utils.Importer;
 
+import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.xpath.XPathExpressionException;
 
 import org.eclipse.jface.dialogs.IDialogSettings;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.IImportWizard;
@@ -21,6 +31,8 @@ import org.eclipse.ui.IImportWizard;
 public class ImportExtensionWizard extends Wizard implements IImportWizard {
 	private static final String EXTERNAL_PROJECT_SECTION = "ExternalProjectImportWizard";
 
+	private static final String IMPORT_EXTENSIONS_TITLE = "Import Extension(s)";
+	
 	private ImportExtensionPage mainPage = null;
 	private String initialPath = null;
 	private IStructuredSelection currentSelection = null;
@@ -55,7 +67,7 @@ public class ImportExtensionWizard extends Wizard implements IImportWizard {
 	
 	@Override
 	public void init(IWorkbench workbench, IStructuredSelection currentSelection) {
-		setWindowTitle("__title__");
+		setWindowTitle(IMPORT_EXTENSIONS_TITLE);
 		setDefaultPageImageDescriptor(IDEWorkbenchPlugin.getIDEImageDescriptor("wizban/importproj_wiz.png"));
 		setCurrentSelection(currentSelection);
 	}
@@ -71,36 +83,55 @@ public class ImportExtensionWizard extends Wizard implements IImportWizard {
 	
 	@Override
 	public boolean performFinish() {
-		try {
-			// platformPath
-			IPath platformPath = FixProjectsUtils.getPlatformPath();
-			if (platformPath == null) {
-				System.out.println("ERROR: platformPath not found!");
-			} else {
-				System.out.println("platformPath=[" + platformPath + "]");
-			}
-			
-
-			// TODO Handle Checkbox Update Classpath
-			
-			
-			// Handle Checkbox Update localExtensions.xml
-			ImportExtensionPage importExtensionPage = getMainPage();
-			boolean result = importExtensionPage.createProjects();
-			if (result) {
+		// get platformPath
+		IPath platformPath = FixProjectsUtils.getPlatformPath();
+		if (platformPath == null) {
+			Activator.logError("ERROR: platformPath not found!", null);
+			return false;
+		} 
+		
+		// Import the projects.
+		ImportExtensionPage importExtensionPage = getMainPage();
+		boolean projectsCreated = importExtensionPage.createProjects();
+		if (!projectsCreated) {
+			return false;
+		}
+		
+		// Update localextensions.xml
+		if (importExtensionPage.getUpdateLocalExtensions()) {
+			try {
 				FixProjectsUtils.updateLocalExtensions(importExtensionPage.getCreatedProjects());
+			} catch (XPathExpressionException | ParserConfigurationException | SAXException | IOException | TransformerException exception) {
+				exception.printStackTrace();
+				return false;
 			}
+		}
+		
+		// Fix classpath
+		File platformDir = new File(platformPath.toString());
+		IRunnableWithProgress runnableWithProgress = new IRunnableWithProgress() {
 			
-		} catch (XPathExpressionException exception) {
-			exception.printStackTrace();
-		} catch (ParserConfigurationException exception) {
-			exception.printStackTrace();
-		} catch (SAXException exception) {
-			exception.printStackTrace();
-		} catch (IOException exception) {
-			exception.printStackTrace();
-		} catch (TransformerException exception) {
-			exception.printStackTrace();
+			public void run(IProgressMonitor progressMonitor ) throws InvocationTargetException {
+				if (importExtensionPage.getFixClasspath()) {
+					try {
+						new Importer().resetProjectsFromLocalExtensions(platformDir, progressMonitor, importExtensionPage.getFixClasspath(), false, false, false, false);
+						
+					} catch (CoreException exception) {
+						exception.printStackTrace();
+					}
+				}
+			}
+		};
+
+		
+		try {
+			new ProgressMonitorDialog(getContainer().getShell()).run(true, false, runnableWithProgress);
+		}
+		catch( InvocationTargetException | InterruptedException exception) {
+			Activator.logError("Failed to import the extension(s)", exception);
+			Throwable throwable = (exception instanceof InvocationTargetException) ? exception.getCause() : exception;
+			MessageDialog.openError(getMainPage().getControl().getShell(), "Error", throwable.toString());
+			return false;
 		}
 		
 		return true;
