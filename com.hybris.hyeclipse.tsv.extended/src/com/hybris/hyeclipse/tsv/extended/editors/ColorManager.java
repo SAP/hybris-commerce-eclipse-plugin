1a155b5754c6b2d4af10c25dd855f1371fae5732
Pawel Wolanski <pawel.wolanski@sap.com>
2020-11-26 22:16:07 +0100

    upgrade eclipse platform

---
+++ com.hybris.hyeclipse.tsv.extended/src/com/hybris/hyeclipse/tsv/extended/editors/ColorManager.java
@@ -0,0 +1,26 @@
+package com.hybris.hyeclipse.tsv.extended.editors;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.widgets.Display;
+
+public class ColorManager {
+
+	protected Map<RGB, Color> fColorTable = new HashMap<>(10);
+
+	public void dispose() {
+		fColorTable.values().forEach(Color::dispose);
+	}
+	
+	public Color getColor(RGB rgb) {
+		Color color = fColorTable.get(rgb);
+		if (color == null) {
+			color = new Color(Display.getCurrent(), rgb);
+			fColorTable.put(rgb, color);
+		}
+		return color;
+	}
+}
